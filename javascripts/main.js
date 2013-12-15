e54f8f20ecb90ea0e028d01653acc76a82b35283
shan zhou <hyspace@gmail.com>
2013-12-16 02:49:15 +0800

    add page and tests

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+  $('input').typeahead({
+    name: 'repos'
+  , local: ['Bootstrap 3', 'Bootstrap 2.3.2', 'Bootstrap by Twitter']
+  });
+
+  var haunt = ghostwriter.haunt({
+    input: '#example'
+  , interval: 500
+  , manuscript: [
+      ghostwriter.noop
+    , 'Twi'
+    , ghostwriter.backspace.repeat(3)
+    , 'Boots'
+    , ghostwriter.down.repeat(1)
+    , ghostwriter.enter
+    , ghostwriter.selectAll
+    ]
+  });
+
+  $(document).on('click keydown', function(e) {
+    !e.ghostwriter && haunt.pause();
+  });
+
+  haunt.start();
+
+});
